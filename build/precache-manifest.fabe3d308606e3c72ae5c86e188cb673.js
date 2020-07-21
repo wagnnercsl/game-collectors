74c78dd81c586b2708a1056e714d38ed84f28a4c
Wagnner <wagnner.leivas@gmail.com>
2020-07-20 23:22:04 -0300

    add build

---
+++ build/precache-manifest.fabe3d308606e3c72ae5c86e188cb673.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91050937eb5c06a812068c6637841ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "affc58eb8043966df11c",
+    "url": "/static/css/main.6d3f0ca4.chunk.css"
+  },
+  {
+    "revision": "0a931e870027bb606faf",
+    "url": "/static/js/2.93f55ef5.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.93f55ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "affc58eb8043966df11c",
+    "url": "/static/js/main.3739eb03.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "1bf9d89451b0a618b6d1f16271770f27",
+    "url": "/static/media/home-background.1bf9d894.jpg"
+  },
+  {
+    "revision": "43193307f77a5d04a1dbddeca94c72f2",
+    "url": "/static/media/logo.43193307.png"
+  }
+]);
